1393b6705b11e7730e290d53a3166a1eb17b651f
Olga <alonsoalvarezolga@gmail.com>
2013-08-04 11:08:38 +0200

    pieces views & routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Warehousese3d::Application.routes.draw do
 
   resources :assemblies
+  resources :pieces
+
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
